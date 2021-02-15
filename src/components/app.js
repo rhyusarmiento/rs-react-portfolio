8067160393496db2ff3c2b4a0a161a2f7730084b
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-15 12:16:16 -0700

    Update README

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import axios from 'axios';
+// import axios from 'axios';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <h2>React Rrrrrrrrrrrredux Rrrrrrrrrrrrrrouter</h2>
+        <h2>React Redux Router</h2>
       </div>
     );
   }
