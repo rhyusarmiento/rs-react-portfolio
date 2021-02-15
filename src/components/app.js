b3cd73cbf348df8efae1c07780969a577977ba30
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-15 12:05:25 -0700

    Inital Commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h2>React Rrrrrrrrrrrredux Rrrrrrrrrrrrrrouter</h2>
+      </div>
+    );
+  }
+}
