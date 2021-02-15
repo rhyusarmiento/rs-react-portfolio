e9732de046de6630294475efd31b48c40d05ca76
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-15 16:54:38 -0700

    React Constructor Overview

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,6 +3,11 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+        console.log('hi')
+    }
+
     render() {
         return (
             <div>
