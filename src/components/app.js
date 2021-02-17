8bb09e8b3d766b2946bc42c6e83161a180c7aa23
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-17 11:55:17 -0700

    How to Use Ternary Operators in JSX Code to Implement Conditional Behavior

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import moment from "moment";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import Naviagtion from './navigation/navigation-container'
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
+        <Naviagtion />
         <h2>React Redux Router</h2>
         <h1>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h1>
         <PortfolioContainer/>
