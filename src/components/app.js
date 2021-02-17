0213d3d72b010563267d07cc1f2adf13fdb3a2b7
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-17 12:19:37 -0700

    Basic Route Setup in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 import PortfolioContainer from "./portfolio/portfolio-container";
-import Naviagtion from './navigation/navigation-container'
+import NaviagtionContainer from './navigation/navigation-container'
+import Home from './pages/home'
+import About from './pages/about'
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
-        <Naviagtion />
+        <Router>
+          <div>
+            <NaviagtionContainer />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+            </Switch>
+          </div>
+        </Router>
         <h2>React Redux Router</h2>
         <h1>{moment().format('MMMM Do YYYY, h:mm:ss a')}</h1>
         <PortfolioContainer/>
