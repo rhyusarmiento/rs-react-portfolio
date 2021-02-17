b1bfc48a39b400cbfe394ef4daa2de7c11043897
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-17 14:33:54 -0700

    How to Access URL Values in React

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,7 @@ import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
 import Blog from './pages/blog'
+import PortfolioDetail from './portfolio/portfolio-detail'
 
 export default class App extends Component {
   render() {
@@ -26,6 +27,7 @@ export default class App extends Component {
               <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
             </Switch>
           </div>
         </Router>
