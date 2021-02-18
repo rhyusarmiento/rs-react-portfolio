55cf7394f77a3c0f23c4ab9cb2bf84a323f319f7
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-18 13:21:40 -0700

    Building Out the Flexbox Container for the Navigation Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -10,21 +10,26 @@ export default class NavigationComponent extends Component {
 
     render() {
         return (
-            <div>
-                <NavLink exact to="/" activeClassName="nav-link-active">
-                    Home
-                </NavLink>
-                <NavLink to="/about">
-                    About
-                </NavLink>
-                <NavLink to="/contact">
-                    Contact 
-                </NavLink>
-                <NavLink to="/blog">
-                    Blog 
-                </NavLink>
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <NavLink exact to="/" activeClassName="nav-link-active">
+                        Home
+                    </NavLink>
+                    <NavLink to="/about">
+                        About
+                    </NavLink>
+                    <NavLink to="/contact">
+                        Contact 
+                    </NavLink>
+                    <NavLink to="/blog">
+                        Blog 
+                    </NavLink>
 
-                {false ? <button>Add Blog</button> : null }
+                    {false ? <button>Add Blog</button> : null }
+                </div>
+                <div className="right-side">
+                    RHYU SARMIENTO
+                </div>
             </div>
         )
     }
