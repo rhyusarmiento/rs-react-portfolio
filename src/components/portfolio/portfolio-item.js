ae3a94f743aa39bd7861e0228f7923ce530d0da3
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-18 14:36:57 -0700

    How to Use Inline Styles in React to Render Background Images

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -6,8 +6,14 @@ export default function(props) {
   const {id, description, thumb_image_url, logo_url } = props.item;
   return (
     <div className="portfolio-item-wrapper">
-      <img src={thumb_image_url} />
-      <img src={logo_url} />
+      <div 
+        className="portfolio-img-background"
+        style={{
+          // backgroundImage: "url(" + thumb_image_url + ")"
+          backgroundImage: `url(${thumb_image_url})`
+        }}
+      />
+      {/* <img src={logo_url} /> */}
       <div>{description}</div>
       <Link to={`./portfolio/${id}`}>
         Link
