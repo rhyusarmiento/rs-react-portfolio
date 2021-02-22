15ad9e5c3c177bbc286efc549954155b53c5df48
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-02-22 15:30:06 -0700

    Introduction to Render Props in React

--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,29 @@ import PortfolioDetail from './portfolio/portfolio-detail'
 import NoMatch from './pages/no-match'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    }
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this)
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this)
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+  
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
   render() {
     return (
       <div className="container">
@@ -26,7 +49,16 @@ export default class App extends Component {
               <Route path="/about" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              <Route path="/auth" component={Auth} />
+              <Route 
+                path="/auth"
+                render={(props) => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+                )}
+              />
               <Route 
                 exact
                 path="/portfolio/:slug" 
