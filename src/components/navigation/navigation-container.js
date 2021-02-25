35c52621e136c968b61a72d9be3a6f8c9197282b
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-02-25 11:02:49 -0700

    name change

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -62,7 +62,7 @@ const NavigationComponent = props => {
       </div>
 
       <div className="right-side">
-        JORDAN HUDGENS
+        RHYU SARMIENTO
         {props.loggedInStatus === "LOGGED_IN" ? (
           <a onClick={handleSignOut}>Sign Out</a>
         ) : null}
