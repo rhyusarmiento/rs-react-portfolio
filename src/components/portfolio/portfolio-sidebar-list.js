bb4c7018f780ebf704ee47b4ee51f9e185833382
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-02-25 14:09:42 -0700

    Installing and Configuring Font Awesome in React

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function PortfolioSidebarList(props) {
     const portfolioList = props.data.map(
@@ -11,7 +12,7 @@ function PortfolioSidebarList(props) {
                     <h1 className="title">{portfolioItem.name}</h1>
                     <h2>{portfolioItem.id}</h2>
                     <a onClick={() => props.handleDeleteClick(portfolioItem)}>
-                        Delete
+                        <FontAwesomeIcon icon="trash" />
                     </a>
                 </div>
             );
