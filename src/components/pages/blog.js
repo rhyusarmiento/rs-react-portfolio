572dde2e3b519e6717b828c273e6c1505590f89d
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-02 13:30:17 -0700

    Rendering the React Modal in the Blog Component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import BlogItem from '../blog/blog-item';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import BlogModal from './../modals/blog-modal'
 
 export default class Blog extends Component {
     constructor() {
@@ -68,6 +68,7 @@ export default class Blog extends Component {
 
         return (
             <div className="blog-container">
+                <BlogModal />
                 <div className="content-container">
                     {blogRecords}
                 </div>
