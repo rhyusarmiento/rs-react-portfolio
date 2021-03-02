776990fab72a28d02ed11a0ee7d7e301f0d8d6d3
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-02 12:05:57 -0700

    Building the Full Infinite Scroll Functionality in React

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import BlogItem from '../blog/blog-item';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export default class Blog extends Component {
     constructor() {
         super();
         this.state = {
-            blogItems: []
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0,
+            isLoading: true
         }
 
         this.getBlogItems = this.getBlogItems.bind(this)
@@ -16,18 +21,32 @@ export default class Blog extends Component {
 
     activateInfiniteScoll() {
         window.onscroll = () => {
-            console.log("scroll");
+            if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+                return;
+            }
+            if (
+                window.innerHeight + document.documentElement.scrollTop ===
+                document.documentElement.offsetHeight
+            ) {
+                this.getBlogItems()
+            }
         }
     }
 
     getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        })
         axios
-            .get("https://rs.devcamp.space/portfolio/portfolio_blogs", {
+            .get(`https://rs.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
                 withCredentials: true
             })
             .then(response => {
+                console.log("res", response);
                 this.setState({
-                    blogItems: response.data.portfolio_blogs
+                    blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+                    totalCount: response.data.meta.total_records,
+                    isLoading: false
                 });
             })
             .catch(error => {
@@ -49,6 +68,11 @@ export default class Blog extends Component {
                 <div className="content-container">
                     {blogRecords}
                 </div>
+                {this.state.isLoading ? (
+                    <div className="content-loader">
+                        <FontAwesomeIcon icon="spinner" spin />
+                    </div>
+                ) : null}
             </div>
         )
     }
