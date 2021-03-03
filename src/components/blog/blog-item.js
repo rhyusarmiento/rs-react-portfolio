e6a82809ee9200c5b2a303fec5e7e142e732a625
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-03 13:25:58 -0700

    Removing HTML Tags and Generating Summaries for Blogs

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import striptags from "striptags";
+import Truncate from "react-truncate";
 
 export default function BlogItem(props) {
     const {
@@ -16,8 +18,17 @@ export default function BlogItem(props) {
                 <h1>{title}</h1>
             </Link>
             <div>
-                {content}
-            </div>
+            <Truncate
+                lines={5}
+                ellipsis={
+                    <span>
+                    ...<Link to={`/b/${id}`}>Read more</Link>
+                    </span>
+                }
+            >
+                {striptags(content)}
+            </Truncate>
+        </div>
         </div>
     )
 }
