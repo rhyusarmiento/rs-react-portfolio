3a951cba2148dca7155c4b20b8f3fbe6af6c553c
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-04 13:42:40 -0700

    Adding Click Handler to Render the BlogForm Component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { Component } from 'react'
 import ReactHtmlParser from "react-html-parser"
+
+import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
@@ -8,10 +10,17 @@ export default class BlogDetail extends Component {
         super(props)
         this.state = {
             currentId: this.props.match.params.slug,
-            blogItem: {}
+            blogItem: {},
+            editMode: false
         }
 
         this.getBlogItem = this.getBlogItem.bind(this)
+        this.handleEditClick = this.handleEditClick.bind(this);
+    }
+
+    handleEditClick() {
+        console.log("handle edit clicked");
+        this.setState({ editMode: true });
     }
 
     getBlogItem() {
@@ -39,13 +48,23 @@ export default class BlogDetail extends Component {
             blog_status
         } = this.state.blogItem
 
+        function contentManager() {
+            if (this.state.editMode) {
+                return <BlogForm />
+            } else {
+                return (
+                    <div className="content-container">
+                        <h1 onClick={this.handleEditClick}>{title}</h1>
+                        <BlogFeaturedImage img={featured_image_url} />
+                        <div className="content">{ReactHtmlParser(content)}</div>
+                    </div>
+                )
+            }
+        }
+
         return (
             <div className="blog-container">
-                <div className="content-container">
-                    <h1>{title}</h1>
-                    <BlogFeaturedImage img={featured_image_url} />
-                    <div className="content">{ReactHtmlParser(content)}</div>
-                </div>
+                {contentManager()}
             </div>
         )
     }
