f42bd43124d29fbea0e0d3d683e0b96948a0821d
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-05 11:54:55 -0700

    Enabling the Ability to Delete Blog Post

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -23,6 +23,27 @@ export default class Blog extends Component {
         this.handleNewBlogClick = this.handleNewBlogClick.bind(this)
         this.handleModalClose = this.handleModalClose.bind(this);
         this.handleSuccessfullNewBlogSubmission = this.handleSuccessfullNewBlogSubmission.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+
+    handleDeleteClick(blog) {
+        axios
+          .delete(
+            `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+            { withCredentials: true }
+          )
+          .then(response => {
+            this.setState({
+              blogItems: this.state.blogItems.filter(blogItem => {
+                return blog.id !== blogItem.id;
+              })
+            });
+    
+            return response.data;
+          })
+          .catch(error => {
+            console.log("delete blog error", error);
+          });
     }
 
     handleSuccessfullNewBlogSubmission(blog) {
@@ -61,7 +82,9 @@ export default class Blog extends Component {
             currentPage: this.state.currentPage + 1
         })
         axios
-            .get(`https://rs.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
+            .get(`https://rs.devcamp.space/portfolio/portfolio_blogs?page=${
+                this.state.currentPage
+            }`, {
                 withCredentials: true
             })
             .then(response => {
@@ -86,7 +109,18 @@ export default class Blog extends Component {
 
     render() {
         const blogRecords = this.state.blogItems.map(blogItem => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+            if (this.props.loggedInStatus === "LOGGED_IN") {
+                return (
+                    <div key={blogItem.id} className="admin-blog-wrapper">
+                        <BlogItem blogItem={blogItem} />
+                        <a onClick={() => this.handleDeleteClick(blogItem)}>
+                            <FontAwesomeIcon icon="trash" />
+                        </a>
+                    </div>
+                );
+            } else {
+                return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+            }
         });
 
         return (
